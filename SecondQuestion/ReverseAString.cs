5d6737c39eee1bc9663c35cd38749e13c05c18fd
Fay-OK <Farzaneh.orak@gmail.com>
2017-03-05 22:05:27 -0800

    SecondQuestion Setup. Reverse a null-terminated string

---
+++ SecondQuestion/ReverseAString.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SecondQuestion
+{
+    class ReverseAString
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
