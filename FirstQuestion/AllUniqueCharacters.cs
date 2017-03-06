26c3c70b0879db2ce9613d4902f8bb3fcee6415b
Fay-OK <Farzaneh.orak@gmail.com>
2017-03-05 21:58:49 -0800

    Solved first question: If a string conatains all the unique characters.

---
+++ FirstQuestion/AllUniqueCharacters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstInterviewQuestion {
+    class AllUniqueCharacters {
+        static void Main (string[] args) {
+            var strValue = "qwertyuiopasdfghakaiqjklmnbvcxz12344";
+            IsUniqueChar (strValue);
+        }
+
+        private static void IsUniqueChar (String strValue) {
+            HashSet<int> hs = new HashSet<int> ();
+            for (int i = 0; i < strValue.Length; i++) {
+                if (hs.Contains (strValue[i])) {
+                    System.Console.WriteLine ("Dulplicate --> " + strValue[i]);
+                }
+                hs.Add (strValue[i]);
+            }
+        }
+    }
+}
