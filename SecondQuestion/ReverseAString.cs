64b311d1e95231dd0430aab49674e651519bf3fe
Fay-OK <Farzaneh.orak@gmail.com>
2017-03-08 18:34:11 -0800

    Problem solved. Added two functions using library and w/o library.

--- SecondQuestion/ReverseAString.cs
+++ SecondQuestion/ReverseAString.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
-namespace SecondQuestion
-{
-    class ReverseAString
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace SecondQuestion {
+    class ReverseAString {
+        static void Main (string[] args) {
+            GetReverseString ("Farzaneh is so cool!\0");
+            getStringWithoutLib ("Farzaneh is so cool!\0");
+        }
+
+        private static void GetReverseString (string str) {
+            char[] myString = str.ToCharArray ();
+            Array.Reverse (myString);
+            Console.WriteLine (myString);
+        }
+
+        private static void getStringWithoutLib (string str) {
+            char[] strArray = new char[str.Length];
+
+            var stringLength = str.Length - 1;
+            for (int i = str.Length - 1; i >= 0; i--) {
+                strArray[stringLength - i] = str[i];
+            }
+            Console.WriteLine (strArray);
         }
     }
 }
