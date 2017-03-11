404573bf448eff5086bcdd92301dfaba484877b4
Fay-OK <Farzaneh.orak@gmail.com>
2017-03-10 16:54:20 -0800

    Find permutation in two strings. Problem Solved.

--- ThirdQuestion/Program.cs
+++ ThirdQuestion/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Linq;
 
-namespace ThirdQuestion
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace ThirdQuestion {
+    class StringPermutation {
+        static void Main (string[] args) {
+            var firstString = "wertyuiopq".ToUpper ();
+            var secondString = "pdoiuytrewq".ToUpper ();
+            FindPermutation (firstString, secondString);
+        }
+
+        private static void FindPermutation (string firstString, string secondString) {
+            if (firstString.Length != secondString.Length) {
+                System.Console.WriteLine ("The Two Strings Are Not Permuted");
+                return;
+            }
+
+            char[] firstStringArray = firstString.ToCharArray ();
+            char[] secondStringArray = secondString.ToCharArray ();
+
+            Array.Sort (firstStringArray);
+            Array.Sort (secondStringArray);
+
+            if (firstStringArray.SequenceEqual (secondStringArray)) {
+                System.Console.WriteLine("The Two Strings Are Permuted");
+            }
         }
     }
 }
