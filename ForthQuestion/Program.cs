0ca23b4df3e8e02a9b3b51aa3987c4945e847599
Fay-OK <Farzaneh.orak@gmail.com>
2017-03-14 07:58:35 -0700

    Setup for question four.

---
+++ ForthQuestion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ForthQuestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
