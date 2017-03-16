c63cc1bd523f1a88ca8d4f4068946b7c661167b6
Fay-OK <Farzaneh.orak@gmail.com>
2017-03-15 17:35:22 -0700

    Replaced the spaces in the string with %20 using two diff approaches.

--- ForthQuestion/Program.cs
+++ ForthQuestion/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 
-namespace ForthQuestion
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace ForthQuestion {
+    class SpaceReplacer {
+        static void Main (string[] args) {
+            var str = "Mr John Smith    ";
+            var strLength = 13;
+            doReplaceWithBuiltInFunc (str);
+            doReplaceWithoutBuiltInFunc (str, strLength);
+        }
+
+        private static void doReplaceWithoutBuiltInFunc (string str, int strLength) {
+            string[] newString = new string[strLength];
+            const string replacedChar = "%20";
+
+            for (int i = 0; i < strLength; i++) {
+                int asciCode = str[i];
+                newString[i] = str[i].ToString ();
+
+                if (asciCode == 32) {
+                    newString[i] = replacedChar;
+                    Console.WriteLine (asciCode);
+                }
+
+            }
+            Console.WriteLine (string.Join ("", newString));
+
+        }
+
+        private static void doReplaceWithBuiltInFunc (string str) {
+            var replacedSpaces = str.Trim ().Replace (" ", "%20");
+            Console.WriteLine (replacedSpaces);
         }
     }
 }
