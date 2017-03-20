9caae90e78ac17cd1a21d23fd24969821e9a38b2
Farzaneh Orak <Farzaneh.Orak@hunterindustries.com>
2017-03-20 08:07:25 -0700

    Question five is added. Word compressor

---
+++ FifthQuestion/FifthQuestion/Compression.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifthQuestion
+{
+    class Compression
+    {
+        static void Main(string[] args)
+        {
+            var newValue= " ";
+            var str = "ddrrewwws";
+            Wordcompressor wp = new Wordcompressor();
+            newValue = wp.GetCompress(str);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FifthQuestion/FifthQuestion/WordCompresser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FifthQuestion
+{
+    internal class Wordcompressor
+    {
+        private Dictionary<char, int> charDic = new Dictionary<char, int>();
+        private string newStringWithValues;
+
+        internal string GetCompress(string str)
+        {
+            CreateDictionary(str);
+            newStringWithValues = PrintDictionary();
+            if (newStringWithValues.Length > str.Length)
+            {
+                return str;
+            }
+            return newStringWithValues;
+        }
+
+
+        private string PrintDictionary()
+        {
+            var newString = "";
+            foreach (var kvp in charDic)
+            {
+                newString += kvp.Key;
+                newString += kvp.Value;
+            }
+            Console.WriteLine(string.Join("", charDic.Select(kv => kv.Key + "" + kv.Value)));
+            return newString;
+        }
+
+        private void CreateDictionary(string str)
+        {
+            for (var i = 0; i < str.Length; i++)
+            {
+                if (!charDic.ContainsKey(str[i]))
+                {
+                    charDic.Add(str[i], 1);
+                }
+                else
+                {
+                    charDic[str[i]] += 1;
+                }
+
+            }
+        }
+    }
+}
